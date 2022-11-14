81953267136c8cc331af39054f884b5c3d1cfade
Jac Wynn <jacquinwynn91@gmail.com>
2022-11-13 20:06:11 -0500

    build out header top bar

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { FaPhone } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <TopBar />
+    </header>
   )
+}
+
+const TopBar = () => {
+    return (
+        <section className="bg-[#FCA223]">
+            <container className="flex items-center justify-between max-w-6xl mx-auto p-2.5">
+                <div>
+                    <a href="tel:9045340486" className="text-white font-abzee"><FaPhone className="inline-block mr-[0.625em]" /><span>(904) 534-0486</span></a>
+                </div>
+                <div>
+                    <Button />
+                </div>
+            </container>
+        </section>
+    )
+}
+
+// Move this into its on component folder
+const Button = ({ ctaText="Contact Us" }) => {
+    return (
+        <button className="font-abzee bg-[#252B70] rounded-[3.125rem] px-12 py-4 text-white uppercase tracking-widest">
+            <a href="#">{ ctaText }</a>
+        </button>
+    )
 }
 
 export default Header
