1c83854423d791822431fe61200bfad2cae311ad
Jac Wynn <jacquinwynn91@gmail.com>
2022-11-13 19:31:06 -0500

    add link tags for fonts

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ export default function Home() {
         <title>Professional Cleaning Xperts</title>
         <meta name="description" content="We’re a cleaning company based in Jacksonville, FL. Our professional and experienced cleaning staff does the job right the first time, giving you confidence that your office and other buildings will always look their best and make a great first impression on your clients." />
         <link rel="icon" href="/favicon.png" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=ABeeZee&family=Playfair+Display:wght@600&display=swap" rel="stylesheet"></link>
       </Head>
       <Header />
     </>
