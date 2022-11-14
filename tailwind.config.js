5f5e45714139aaeef9bc17408f7f32a06bca6e66
Jac Wynn <jacquinwynn91@gmail.com>
2022-11-13 19:29:07 -0500

    add fonts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'abzee': ['ABeeZee', 'sans-serif'],
+        'playfair': ['Playfair Display', 'serif']
+      }
+    },
   },
   plugins: [],
 }
